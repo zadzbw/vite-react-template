5ae7aeca857615009c7e67a83f6ca1fcd0da5bec
zadzbw <609842385@qq.com>
2023-04-27 22:05:19 +0800

    feat: 🎸 complete style

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,10 +1,18 @@
-import React, { useState } from 'react'
+import React, { useCallback, useState } from 'react'
 
 export const Counter = () => {
   const [count, setCount] = useState(0)
 
+  const handleClick = useCallback(() => {
+    setCount((v) => v + 1)
+  }, [])
+
   return (
-    <button className="rounded-lg" onClick={() => setCount((v) => v + 1)}>
+    <button
+      className="rounded-lg border border-transparent bg-gray-100 px-4 py-2 font-medium transition-[border-color] duration-200
+                 hover:border-[#646cff]"
+      onClick={handleClick}
+    >
       count is {count}
     </button>
   )
